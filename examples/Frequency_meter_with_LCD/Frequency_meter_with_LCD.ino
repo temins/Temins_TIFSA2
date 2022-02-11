7e3e4293a711606133f12ad525dc5a429dea3aaa
temins <98459093+temins@users.noreply.github.com>
2022-02-11 14:00:35 +0700

    Add files via upload

--- examples/Frequency_meter_with_LCD/Frequency_meter_with_LCD.ino
+++ examples/Frequency_meter_with_LCD/Frequency_meter_with_LCD.ino
@@ -2,17 +2,15 @@
 #include <LiquidCrystal_I2C.h>
 #include <Temins_TIFSA1.h>
 
-Temins_TIFSA1 sensorFreq(2); //Interrupt pin used to connect to sensor. Only use interrupt pin.
+Temins_TIFSA1 sensorFreq(2); //Pin used to connect to sensor. This example use pin 2
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   lcd.init();
-  // Print a message to the LCD.
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("TIFSA1");
+  lcd.print("Temins TIFSA1");
   lcd.setCursor(0, 1);
   lcd.print("Frequency Sensor");
   delay(1000);
@@ -20,10 +18,11 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   float periode = sensorFreq.getPeriod();
   float frekuensi = sensorFreq.getFrequency();
+  Serial.print("Periode : ");
   Serial.println(periode);
+  Serial.print("Frekuensi : ");
   Serial.println(frekuensi);
   lcd.setCursor(0, 0);
   lcd.print("Freq:");
